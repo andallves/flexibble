e743bf3b5c20d175c5af938d9674b71faaad7661
andallves <andrealves10a@gmail.com>
2023-07-17 16:30:45 -0300

    Create LoadMore component

--- lib/actions.ts
+++ lib/actions.ts
@@ -55,13 +55,10 @@ const makeGraphQLRequest = async (query: string, variables = {}) => {
   }
 };
 
-export const fetchAllProjects = (
-  category?: string | null,
-  endcursor?: string | null,
-) => {
+export const fetchAllProjects = (category?: string, endCursor?: string) => {
   client.setHeader('x-api-key', apiKey);
 
-  return makeGraphQLRequest(projectsQuery, { category, endcursor });
+  return makeGraphQLRequest(projectsQuery, { category, endCursor });
 };
 
 export const createNewProject = async (
