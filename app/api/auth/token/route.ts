6e7db1aea1eb8bfb1da83713ad9a2672ebbfc1c2
andallves <andrealves10a@gmail.com>
2023-07-17 20:16:48 -0300

    fix bug

--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -4,11 +4,6 @@ import { NextRequest, NextResponse } from 'next/server';
 const secret = process.env.NEXTAUTH_SECRET;
 
 export async function GET(req: NextRequest) {
-  try {
-    const token = await getToken({ req, secret, raw: true });
-
-    return NextResponse.json({ token }, { status: 200 });
-  } catch (error) {
-    throw new Error('Error to getToken');
-  }
+  const token = await getToken({ req, secret, raw: true });
+  return NextResponse.json({ token }, { status: 200 });
 }
